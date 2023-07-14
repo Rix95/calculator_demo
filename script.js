de28beeb7e4a0170556ddc89bdaaee7f5d3b5baa
RixDota <ricalrax@gmail.com>
2023-07-14 01:18:26 -0500

    Fixed bugs implemented CE C Del

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ let positive = true;
 let deleteContent = false;
 
 // add constant to all document query selectors and eleennts
-const numberButtons = document.querySelectorAll([number]);
-const operatorButtons = document.querySelectorAll([operator]);
+const numberButtons = document.querySelectorAll("[number]");
+const operatorButtons = document.querySelectorAll("[operator]");
+const testButton = document.querySelector("[test]");
 const signButton = document.getElementById("sign");
 const deleteButton = document.getElementById("delete");
 const clearButton = document.getElementById("clear");
@@ -14,17 +15,44 @@ const ceButton = document.getElementById("ce");
 const pointButton = document.getElementById("point");
 const equalsButton = document.getElementById("equals");
 //screens
-const cacheOperation = document.getElementById("cache-operation");
-const currentOperation = document.getElementById("current-operation");
+const cacheOperation = document.getElementById("cacheoperation");
+const currentOperation = document.getElementById("currentoperation");
 
 // #set up event listeners for all buttons
 numberButtons.forEach((button) =>
-  button.addEventListener("click", () => appendNNumber(button.textContent))
+  button.addEventListener("click", () => appendNumber(button.textContent))
 );
 
 operatorButtons.forEach((button) =>
   button.addEventListener("click", () => setOperator(button.textContent))
 );
+
+// signButton.addEventListener("click", changeSign);
+ceButton.addEventListener("click", clearEntry);
+clearButton.addEventListener("click", clearEverything);
+deleteButton.addEventListener("click", deleteNumber);
+pointButton.addEventListener("click", addPoint);
+
+equalsButton.addEventListener("click", checkOperation);
+
+function clearEntry() {
+  currentOperation.textContent = "0";
+}
+
+function clearEverything() {
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = "";
+  currentOperation.textContent = "0";
+  cacheOperation.textContent = "";
+}
+
+function deleteCurrentScreen() {
+  currentOperation.textContent = "";
+  deleteContent = false;
+}
+
+function checkOperation() {}
 
 function appendNumber(number) {
   if (currentOperation.textContent === "0" || deleteContent)
@@ -50,42 +78,42 @@ function addPoint() {
 }
 
 function deleteNumber() {
-  currentOperation.textContent = currentOperation
+  currentOperation.textContent = currentOperation.textContent
     .toString()
-    .textContent.slice(0, -1);
+    .slice(0, -1);
 }
 
-function add(x, y) {
-  return x + y;
-}
+// function add(x, y) {
+//   return x + y;
+// }
 
-function subtract(x, y) {
-  return x - y;
-}
+// function subtract(x, y) {
+//   return x - y;
+// }
 
-function multiply(x, y) {
-  return x * y;
-}
+// function multiply(x, y) {
+//   return x * y;
+// }
 
-function divide(x, y) {
-  if (y === 0) {
-    return "You can't divide by zero!"; /* should add alert, not return also clear display */
-  }
-  return x / y;
-}
+// function divide(x, y) {
+//   if (y === 0) {
+//     return "You can't divide by zero!"; /* should add alert, not return also clear display */
+//   }
+//   return x / y;
+// }
 
-function operate(operator, x, y) {
-  switch (operator) {
-    case "+":
-      return add(x, y);
-      break;
-    case "-":
-      return subtract(x, y);
-      break;
-    case "*":
-      return multiply(x, y);
-      break;
-    case "/":
-      return divide(x, y);
-  }
-}
+// function operate(operator, x, y) {
+//   switch (operator) {
+//     case "+":
+//       return add(x, y);
+//       break;
+//     case "-":
+//       return subtract(x, y);
+//       break;
+//     case "*":
+//       return multiply(x, y);
+//       break;
+//     case "/":
+//       return divide(x, y);
+//   }
+// }
